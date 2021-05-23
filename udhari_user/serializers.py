f4bd8203567b335242c937363fd14dd28bd9f09e
Ashutosh Singh <thecodepapaya@gmail.com>
2021-05-24 01:25:39 +0530

    Implement explicit auth_token approach

--- udhari_user/serializers.py
+++ udhari_user/serializers.py
@@ -1,11 +1,31 @@
+from hashlib import sha256
+
+from django.utils.datetime_safe import datetime
 from rest_framework import serializers
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
+
 from .models import UdhariUser
 
 
-class UdhariUserSerializer(serializers.ModelSerializer):
+class POSTUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UdhariUser
-        fields = ['name', 'phone_number', 'country_code',
-                  'photo_url', 'uid', 'joined_on', 'fcm_token']
+        fields = '__all__'
+
+    def to_internal_value(self, data):
+        data['auth_token'] = sha256(
+            str(datetime.now()).encode('utf-8')).hexdigest()
+        return data
+
+
+class GETUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UdhariUser
+        fields = [
+            'name',
+            'phone_number',
+            'country_code',
+            'photo_url',
+            'uid',
+        ]
--- udhari_user/views.py
+++ udhari_user/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-
 from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.utils import timezone
@@ -9,15 +6,16 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from udhari_user.models import UdhariUser
-from udhari_user.serializers import UdhariUserSerializer
+from udhari_user.serializers import GETUserSerializer, POSTUserSerializer
 
 
 class user(APIView):
     def get(self, request, pk, format=None):
         try:
             user = UdhariUser.objects.get(pk=pk)
-            serializer = UdhariUserSerializer(user)
+            serializer = GETUserSerializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except UdhariUser.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
@@ -25,18 +23,14 @@ class user(APIView):
 
 class register(APIView):
     def post(self, request, format=None):
-        serializer = UdhariUserSerializer(data=request.data)
-        print(request.data)
+        serializer = POSTUserSerializer(data=request.data)
         if serializer.is_valid():
-            print(request.data)
-            print(serializer.validated_data)
             serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-        print(serializer)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response({'auth_token': serializer.validated_data['auth_token']}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # TODO: remove later
     def get(self, request, format=None):
         users = UdhariUser.objects.all()
-        serializer = UdhariUserSerializer(users, many=True)
+        serializer = POSTUserSerializer(users, many=True)
         return Response(serializer.data)
