fa931ebdfbb39f2d705d70606b48104357eac21b
Ashutosh Singh <thecodepapaya@gmail.com>
2021-09-08 00:16:24 +0530

    Add bill get logic to bill view

--- bill/models.py
+++ bill/models.py
@@ -6,7 +6,7 @@ from udhari_user.models import UdhariUser
 class Bill(models.Model):
     name = models.CharField(max_length=30, default='New Bill')
     total_amount = models.DecimalField(
-        max_digits=5, decimal_places=2, default=0.00)
+        max_digits=6, decimal_places=2, default=0.00)
     notes = models.CharField(max_length=200, blank=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
@@ -20,11 +20,12 @@ class Bill(models.Model):
 
 class BillContributor(models.Model):
     user = models.ForeignKey(UdhariUser, on_delete=models.CASCADE)
-    share = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    share = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     amount_contributed = models.DecimalField(
         max_digits=5, decimal_places=2, default=0.00)
     belongs_to_bill = models.ForeignKey(
         Bill, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user} share: {self.share} contributed: {self.amount_contributed}'
--- bill/views.py
+++ bill/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404, HttpResponse, JsonResponse
 from django.utils import timezone
+from django.db.models import Q
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import mixins, status, viewsets
 from rest_framework.permissions import (SAFE_METHODS, BasePermission,
@@ -8,6 +9,9 @@ from rest_framework.response import Response
 
 from bill.models import Bill, BillContributor
 from bill.serializers import BillContributorSerializer, BillSerializer
+
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 
 class BillPermission(BasePermission):
@@ -20,11 +24,30 @@ class BillPermission(BasePermission):
         return super().has_permission(request, view)
 
 
+class BillContributorPermission(BasePermission):
+    message = "Bill contributor can only be modified by the bill contributor or bill creator. Bill contributor can be viewed by all bill contributors"
+
+    # TODO add permissions
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.user
+
+    def has_permission(self, request, view):
+        return super().has_permission(request, view)
+
+
 class BillViewSet(viewsets.ModelViewSet):
     serializer_class = BillSerializer
-    # permission_classes = [BillPermission]
+    permission_classes = [BillPermission]
     http_method_names = ['get', 'post', 'patch', 'delete', 'head', ]
-    queryset = Bill.objects.all()
+
+    def get_queryset(self):
+        # print(f"Received user: {self.request.user}")
+        contributions = list(BillContributor.objects.filter(user=self.request.user).filter(
+            created_at__gte=date.today() + relativedelta(months=-6)).values('belongs_to_bill').iterator())
+        ids = []
+        for c in contributions:
+            ids.append(c['belongs_to_bill'])
+        return Bill.objects.filter(id__in=ids)
 
 
 class BillContributorViewSet(viewsets.ModelViewSet):
